5d536fcad89fed457b80611f9a8d488d849ec9fa
Kevin Avignon <kevin.o.avignon@gmail.com>
2016-09-08 19:56:31 -0700

    #1 - Added state of character during the game and LifePoints to represent the health during the game

--- A game of light and shadows/GLSAssetLibrary/CharacterInformation.fs
+++ A game of light and shadows/GLSAssetLibrary/CharacterInformation.fs
@@ -1,15 +1,41 @@
 ﻿module CharacterInformation
 
+type CharacterState = 
+    | Alive 
+    | Dead
+
+type LifePoints ={
+    Current : float 
+    Max     : float
+}
+with 
+    member x.isDead = x.Current < 0.0
+
+    member x.capHealth () =
+        if x.Current > x.Max then { Current = x.Max; Max = x.Max } else x
+
+    member x.raiseHealth (lifePoint:int) = 
+        let raisedHealth = { x with Current = x.Current + (lifePoint |> float) }.capHealth()
+        raisedHealth
+
+    member x.takeHit (hitPoint: int) = 
+        let reducedLife = { x with Current = x.Current - (hitPoint |> float) }
+        reducedLife
+
 type CharacterStats = {
-    Health          : float 
+    Health          : LifePoints 
     Speed           : float 
     Strength        : float 
     MagicPower      : float option 
+    Defense         : int
     Resistance      : int
     MagicResist     : float  
     Evade           : int 
     Luck            : int
 }
+with 
+    member x.applyTemporaryDefense (tPoints: int) = 
+        { x with Defense = x.Defense + tPoints }
 
 type CharacterRole = 
     | Wizard 
@@ -19,11 +45,11 @@ type CharacterRole =
     | Sniper 
 
 type CharacterJob = 
-    | Healer        of CharacterRole
-    | Knight        of CharacterRole
-    | Berserker     of CharacterRole
-    | Rider         of CharacterRole
-    | Paladin       of CharacterRole
-    | BowAndBlade   of CharacterRole
-    | Necromancer   of CharacterRole
-    | Nightblade    of CharacterRole
+    | Healer        of CharacterRole  * CharacterStats
+    | Knight        of CharacterRole  * CharacterStats
+    | Berserker     of CharacterRole  * CharacterStats
+    | Rider         of CharacterRole  * CharacterStats
+    | Paladin       of CharacterRole  * CharacterStats
+    | BowAndBlade   of CharacterRole  * CharacterStats
+    | Necromancer   of CharacterRole  * CharacterStats
+    | Nightblade    of CharacterRole  * CharacterStats
