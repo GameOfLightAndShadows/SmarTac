3fd799e046d5bffc63406733b9a1e5cd6948a030
Kevin Avignon <kevin.o.avignon@gmail.com>
2016-09-08 18:38:23 -0700

    #1 - Created basic character information: stats, roles and jobs. Next step: define every jobs with stats and make them a bit random

---
+++ A game of light and shadows/GLSAssetLibrary/CharacterInformation.fs
@@ -0,0 +1,29 @@
+﻿module CharacterInformation
+
+type CharacterStats = {
+    Health          : float 
+    Speed           : float 
+    Strength        : float 
+    MagicPower      : float option 
+    Resistance      : int
+    MagicResist     : float  
+    Evade           : int 
+    Luck            : int
+}
+
+type CharacterRole = 
+    | Wizard 
+    | Knight 
+    | Fighter
+    | MagicSoldier
+    | Sniper 
+
+type CharacterJob = 
+    | Healer        of CharacterRole
+    | Knight        of CharacterRole
+    | Berserker     of CharacterRole
+    | Rider         of CharacterRole
+    | Paladin       of CharacterRole
+    | BowAndBlade   of CharacterRole
+    | Necromancer   of CharacterRole
+    | Nightblade    of CharacterRole
--- A game of light and shadows/GLSAssetLibrary/Library1.fs
+++
@@ -1,4 +0,0 @@
-﻿namespace GLSAssetLibrary
-
-type Class1() = 
-    member this.X = "F#"
