25124e5261abae3537ae17b5ba57cded16a4f38d
Kevin Avignon <kevin.o.avignon@gmail.com>
2016-09-18 13:00:15 -0400

    #56 - Added the shell of message required for the Akka agents. Through time, the message arguments will be updated, some new messages could be added/removed, depending on the state of the project.

--- A game of light and shadows/GLSCore/Protocol.fs
+++ A game of light and shadows/GLSCore/Protocol.fs
@@ -1,2 +1,59 @@
 ﻿module GLSManager.Protocol
 
+type GameManagerProtocol =  
+    | UpdateGlobalState 
+    | DestroyCharacter 
+    | UpdateCharacterHealth 
+    | UpdateCharacterPosition
+    | UpdateCharacterDirection 
+    | StopManager
+
+type BattlePhase = 
+    | Move
+    | EngageCharacter
+    | EndTurn
+
+// Define CharacterObserver which will update the BattleSequenceManager once a character performed an action. 
+
+type BattleSequenceManagerProtocol = 
+    | ChangePhase of BattlePhase
+    | StopManager
+
+type InventoryManagerProtocol = 
+    | LookAtInventory 
+    | ReorderInventory 
+    | TossItem 
+    | AddItem 
+    | SelectItem 
+    | StopManager 
+
+type BattleViewManagerProtocol = 
+    | TryEvade 
+    | UseMeleeAttack 
+    | UseCharacterSpecialMove 
+    | ApplyTemporaryDefenseUpgrade
+    | StopManager 
+
+type StoreManagerProtocol = 
+    | ShowStock 
+    | BuyItem 
+    | SellItem 
+    | StopManager 
+
+type WorldMapManagerProtocol = 
+    | RenderStoryPoint 
+    | MoveCharacterToStoryPoint 
+    | Stop
+
+type CommanadManagerProtocol = 
+    | PerformAttackCommandOn 
+    | PerformMoveCommandWith 
+    | PerformDefendCommandWith 
+    | PerformRotateCommandWith 
+    | PerformEndTurn
+
+type StateServerProtocol =  
+    | UpdateTeamPartyState 
+    | UpdatePartyCharacter 
+    | UpdateGameBoardState
+    | UpdateTeamPartyInventory
