a1b8919f183b8cd240a419067417184d3b5c49db
KevinAvignon <kevin.o.avignon@gmail.com>
2016-10-18 18:24:30 -0400

    #14  -  Implementing a basic brain which will learn from experience based on the SARSA algorithm

--- A game of light and shadows/GLSCore/BrainManager.fs
+++ A game of light and shadows/GLSCore/BrainManager.fs
@@ -1,2 +1,38 @@
 ﻿module GLSManager.BrainManager
 
+open GLSCore.HelperFunctions
+open GLSCore.GameElement
+open System
+
+
+module Brains =
+    type CurrentGameState = PlayerDirection * (GameCell option) list
+
+    type Experience = { State: CurrentGameState; Action: Act; Reward: float; NextState: CurrentGameState } //SARSA
+
+    type Strategy = { State: CurrentGameState; Action: Act } //Possible course of action
+
+    type Brain = Map<Strategy, float> //Strategies tried and rewards collected in the process 
+
+    let randomizer = Random ()
+    let moveChoices = [| Up; Down; Left; Right |]
+    let encounterChoices = [| MeleeAttack; SpecialMove; RaiseDefense; |]
+    let randomDecideMove () = moveChoices.[randomizer.Next(4)]
+    let randomDecideEncounter () = encounterChoices.[randomizer.Next(3)]
+
+
+    let alpha = 0.30 //learning rate
+
+    let learn(brain: Brain) (xp: Experience) = 
+        let s = { State = xp.State; Action = xp.Action }
+        let oStrat = brain |> Map.tryFind s
+        match oStrat with 
+        | Some reward -> 
+            brain |> Map.add (s, 1.0-alpha * reward + alpha * xp.Reward )
+        | None -> 
+            brain |> Map.add (s, alpha * xp.Reward)
+
+    let 
+
+
+
