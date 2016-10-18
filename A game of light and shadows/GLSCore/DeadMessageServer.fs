2ac13b11503071999581853fb8524d5fbcc187e5
KevinAvignon <kevin.o.avignon@gmail.com>
2016-10-18 10:33:02 -0400

    Dead letters are supported.

--- A game of light and shadows/GLSCore/DeadMessageServer.fs
+++ A game of light and shadows/GLSCore/DeadMessageServer.fs
@@ -1,5 +1,21 @@
 ﻿module GLSPersistance.DeadMessageServer
 
-// Akka doesn’t guarantee the delivery of a message
-// The dead letter channel is an event stream which receives all messages that couldn't be processed
-// Unless specified differently, dead letters are logged in the INFO level
+open Akka.Event
+
+open Akka.FSharp
+
+let system = System.create "system" <| Configuration.load ()
+let sysListener (mailbox: Actor<DeadLetter>) = 
+
+    let rec loop () = actor {
+        let! deadLetter = mailbox.Receive ()
+        printfn "SysListner, DeadLetter received: %A" deadLetter.Message
+        return! loop ()
+    }
+    loop ()
+
+let sysListenerRef = spawn system "sysListener" sysListener
+subscribe typeof<DeadLetter> sysListenerRef system.EventStream |> ignore<bool>
+
+let deadActorRef = select "akka://system/user/deadActor" system
+deadActorRef <! "Message to dead actor"
