d43502ccbe57f38c49e454b528cccdbb5477ba9a
KevinAvignon <kevin.o.avignon@gmail.com>
2016-10-18 13:30:03 -0400

    Finish modeling basic game logic to test how a character would learn how to play with available moves and a move range = 1.

--- A game of light and shadows/GLSCore/GameElement.fs
+++ A game of light and shadows/GLSCore/GameElement.fs
@@ -1,6 +1,7 @@
 ﻿module GLSCore.GameElement
 
 open System
+open GLSCore.HelperFunctions
 
 [<Measure>] type abscissa
 [<Measure>] type ordinate
@@ -45,6 +46,18 @@ with
         let reducedLife = { x with Current = x.Current - (hitPoint |> float) }
         reducedLife
 
+
+// Actions 
+type Act = 
+    | MeleeAttack 
+    | ClassAttack 
+    | Left
+    | Right 
+    | Up 
+    | Down
+    | RaiseDefense
+    | EndTurn 
+
 // Direction
 type PlayerDirection = 
     | South
@@ -60,25 +73,12 @@ with
         | West, East -> true 
         | _,_ -> false
 
-// Actions 
-type Act = 
-    | MeleeAttack 
-    | ClassAttack 
-    | Left
-    | Right 
-    | Straight 
-    | Back 
-    | RaiseDefense
-    | Rotate 
-    | EndTurn 
-
-// Position 
-type Pos = {
-    Top: int
-    Left:int
-}
-
-
+    member x.switchOppositeDirection () = 
+        match x with 
+        | South -> North 
+        | North -> South 
+        | East -> West 
+        | West -> East 
 
 // Board Cells 
 type MovementCost = 
@@ -91,13 +91,29 @@ type TileType =
     | Difficult of MovementCost
     | ``Road block`` of MovementCost
 
+type Treasure = 
+    | HealthPotion  
+    | Currency      
+
+type Trap = 
+    | ReduceLifePoints
+    | ReduceMoney     
+
 type GameCell = 
-    | Tile of TileType
-    | Treasure of RandomTreasure
+    | Empty 
+    | CollectibleTreasure of Treasure 
+    | Enemy
+    | HiddenTrap of Trap
+    
+// Game Board Size 
+type MapSize = { Width: int; Height: int }
+
+// Position 
+type Pos = { Top: int; Left:int }
 
 // AI Character
 
-type Character = { Position: Pos; Direction: PlayerDirection; Cell: GameCell }
+type Character = { Position: Pos; Direction: PlayerDirection }
 
 // Game Board 
 
@@ -112,5 +128,72 @@ with
 
 type GameState = { Board: GameBoard ; Characters: Character list; Score: int }
 
-// Game Board Size 
-type MapSize = { Width: int; Height: int }
+// Based on Machine Learning Projects for .NET developpers
+[<AutoOpen>]
+module GameLogic = 
+    [<Literal>]
+    let HEALTHPOTIONSCORE = 50 
+
+    [<Literal>]
+    let CURRENCYSCORE = 100 
+
+    [<Literal>]
+    let REDUCELIFEPOINTSCORE = -125 
+
+    [<Literal>]
+    let REDUCECURRENCYSCORE = -75 
+        
+    let onboard (size: MapSize) (pos: Pos) = {   
+        Top = pos.Top %%% size.Height
+        Left = pos.Left %%% size.Width    
+    }
+
+    let changeDirection (act: Act) (dir: PlayerDirection) = 
+        match act with 
+        | Up 
+        | Down
+        | Right 
+        | Left ->  dir.switchOppositeDirection()
+        | _ -> dir 
+
+    let moveTo 
+        (size: MapSize) 
+        (direction: PlayerDirection)
+        (pos: Pos)  = 
+        match direction with 
+        | North -> { pos with Top = (pos.Top - 1) %%% size.Height }
+        | South -> { pos with Top = (pos.Top + 1) %%% size.Height }
+        | East  -> { pos with Left = (pos.Left + 1) %%% size.Width }
+        | West  -> { pos with Left = (pos.Left - 1) %%% size.Width }
+
+    let applyMoveDecision (mapSize: MapSize) (action: Act) (charac: Character) = 
+        let newDirection = charac.Direction |> changeDirection action 
+        { Position = charac.Position |> moveTo mapSize newDirection
+          Direction = newDirection }
+
+    let computeScoreGain (board: GameBoard) (charac: Character) = 
+        let pos = charac.Position
+        match board.GameMap.TryFind pos with 
+        | Some cell -> 
+            match cell with
+            | Empty 
+            | Enemy -> 0
+            | HiddenTrap t -> 
+                match t with 
+                | ReduceLifePoints -> REDUCELIFEPOINTSCORE
+                | ReduceMoney      -> REDUCECURRENCYSCORE
+            | CollectibleTreasure ct -> 
+                match ct with 
+                | HealthPotion ->  HEALTHPOTIONSCORE
+                | Currency     ->  CURRENCYSCORE
+        | None -> 0
+
+    let updateGameBoard (board: GameBoard) (character: Character) = 
+        board.GameMap |> Map.filter(fun position _ -> position <> character.Position)
+            
+
+
+        
+
+
+
