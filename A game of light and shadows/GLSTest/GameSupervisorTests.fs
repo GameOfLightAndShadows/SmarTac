cb5b804017f4f9d3cf58a58b012842cfac50e650
KevinAvignon <kevin.o.avignon@gmail.com>
2016-10-19 11:38:01 -0400

    Added test files for the first step of the continuous integration architecture with Travis CI

---
+++ A game of light and shadows/GLSTest/GameSupervisorTests.fs
@@ -0,0 +1,7 @@
+﻿module GameSupervisorTests
+
+open NUnit.Framework 
+
+let [<Test>] ``When 2 is added to 2 expect 4``() = Assert.AreEqual(4, 2+2)
+
+let [<Test>] ``10 should equal 10`` () = Assert.AreEqual(10,10)
--- A game of light and shadows/GLSTest/Library1.fs
+++
@@ -1,8 +0,0 @@
-﻿namespace GLSTest
-
-[<TestFixture>]
-type TestClass() = 
-
-    [<Test>]
-    member this.When2IsAddedTo2Expect4() = 
-        Assert.AreEqual(4, 2+2)
