14604410a193b70ec215ddb67553c1b3b3bae0c8
Kevin Avignon <kevin.o.avignon@gmail.com>
2016-11-27 23:47:10 -0500

    #19 - Create domain model of the store’s manager state and a player’s transaction. Every message that will be received by the manager are there too.

--- A game of light and shadows/GLSCore/ItemStoreManager.fs
+++ A game of light and shadows/GLSCore/ItemStoreManager.fs
@@ -1,2 +1,34 @@
 ﻿module GLSManager.ItemStoreManager
 
+open GLSCore.InventoryItems
+open Akka 
+open Akka.Actor
+
+type StoreTransaction = {
+    StoreStock : GameItem list 
+    Bill       : decimal<usd>
+}
+
+type ItemStoreState = {
+    TeamPartyRef : IActorRef
+    PlayerParty  : PartyCharacter array 
+    SelectedCharacter : PartyCharacter option
+    BagTotalWeight : float<kg> 
+    PlayerTotalCurrency : float<usd> 
+    StoreStock   : ( GameItem * ItemCategory) list 
+    Transaction  : StoreTransaction
+}
+
+type ItemStoreProtocol = 
+    | PurchaseMode 
+    | SellMode 
+    | ConfirmPurchase 
+    | ConfirmSell 
+    | SendItemsToInventory of GameItem array
+    | SendItemsToSelectedCharacter of GameItem array
+    | UpdateGameItemQunatity of GameItem 
+    | RemoveItemFromStock of GameItem
+    | ShowHowMuchShouldBeBoughtInstead of GameItem
+    | DisableStoreStock
+    | IncreasePlayerTotalMoney of StoreTransaction
+    | DecreasePlayerTotalMoney of StoreTransaction 
